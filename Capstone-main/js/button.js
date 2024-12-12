c2c8db94166bf4bbcedc8d26df756b78a47fdf1e
romrta1aug8n <romartalaugon123@gmail.com>
2024-12-12 18:39:20 +0800

    first commit

---
+++ Capstone-main/js/button.js
@@ -0,0 +1,26 @@
+function toggleDropdown() {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
+}
+
+function toggleDropdown() {
+    const dropdownContent = document.querySelector('.dropdown-report');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
+}
+
+function toggleDropdown() {
+    const dropdownContent = document.querySelector('.dropdown-corn');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
+}
